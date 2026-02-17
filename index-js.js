d42455111709ddeda2ae360bb8df8b572e615af7
Anzhela DZARASOVA <angelas82@list.ru>
2026-02-17 06:13:27 +0100

    Added viem

--- index-js.js
+++ index-js.js
@@ -1,1 +1,20 @@
 console.log("Start of the Page")
+// https://viem.sh/docs/getting-started
+import { createWalletClient, custom } from 'https://esm.sh/viem';
+let walletClient;
+
+async function connect() {
+    if (window.ethereum !== undefined) {
+        walletClient = createWalletClient({
+            transport: custom(window.ethereum)
+        });
+        const [address] = await walletClient.requestAddresses()
+        connectBunnon.innerHTML = "Connected!";
+        console.log("address:", address);
+    } else {
+        connectBunnon.innerHTML = "Please install MetaMask!";
+    }
+}
+
+const connectBunnon = document.getElementById("connectBunnon");
+connectBunnon.onclick = connect;
