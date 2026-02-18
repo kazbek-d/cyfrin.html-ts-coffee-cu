e6c37fed72c3f006d09acb93f05fdab21dc5dd37
Anzhela DZARASOVA <angelas82@list.ru>
2026-02-18 07:39:16 +0100

    Added Public Client

--- index-js.js
+++ index-js.js
@@ -1,20 +1,64 @@
 console.log("Start of the Page")
 // https://viem.sh/docs/getting-started
-import { createWalletClient, custom } from 'https://esm.sh/viem';
+import { createWalletClient, custom, createPublicClient, http } from 'https://esm.sh/viem';
 let walletClient;
+let publicClient;
 
-async function connect() {
+function makeWalletClient() {
+    walletClient = createWalletClient({
+        transport: custom(window.ethereum)
+    });
+}
+
+async function makePublicClient() {
+    publicClient = createPublicClient({
+        transport: custom(window.ethereum)
+    });
+    await publicClient.simulateContract({
+        address: '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
+        abi: wagmiAbi,
+        functionName: 'mint',
+        account,
+    })
+}
+
+async function connectAndAct(action) {
     if (window.ethereum !== undefined) {
-        walletClient = createWalletClient({
-            transport: custom(window.ethereum)
-        });
-        const [address] = await walletClient.requestAddresses()
+        makeWalletClient();
         connectBunnon.innerHTML = "Connected!";
-        console.log("address:", address);
+        await action();
     } else {
         connectBunnon.innerHTML = "Please install MetaMask!";
     }
 }
 
+async function connect() {
+    async function action() {
+        const [address] = await walletClient.requestAddresses()
+        console.log("address:", address);
+    }
+    await connectAndAct(action);
+}
+
+async function balance() {
+    console.log("balance");
+}
+
+async function fund() {
+    async function action() {
+        await makePublicClient();
+        console.log(`Funding with: ${ethAmountInput.value}`);
+    }
+    await connectAndAct(action);
+}
+
 const connectBunnon = document.getElementById("connectBunnon");
 connectBunnon.onclick = connect;
+
+const balanceBunnon = document.getElementById("balanceBunnon");
+balanceBunnon.onclick = balance;
+
+const fundBunnon = document.getElementById("fundBunnon");
+fundBunnon.onclick = fund;
+
+const ethAmountInput = document.getElementById("ethAmount");
